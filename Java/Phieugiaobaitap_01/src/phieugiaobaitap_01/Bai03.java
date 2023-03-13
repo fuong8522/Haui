73cfd8ef308605518675df2a65bef2288e1a3aa8
fuong <fuong8522@gmail.com>
2023-03-13 10:13:18 +0700

    12/3

--- Java/Phieugiaobaitap_01/src/phieugiaobaitap_01/Bai03.java
+++ Java/Phieugiaobaitap_01/src/phieugiaobaitap_01/Bai03.java
@@ -1,9 +1,25 @@
 package phieugiaobaitap_01;
 
-/**
- *
- * @author Nguyễn Văn Phương
+import java.util.Scanner;
+
+/*
+    @author Nguyễn Văn Phương
+    Bài 3: Viết chương trình tính n!
  */
 public class Bai03 {
     
+    public static long GiaiThua(int n) {
+        long answer = 1;
+        for (int i = 1; i <= n; i++) {
+                answer *= i;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập vào n: ");
+        n = sc.nextInt();
+        System.out.println(n + "! = " + GiaiThua(n) );
+    }
 }
